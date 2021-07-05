c15484a5f337663af1d5f459c4d7261b9a8f34d2
GunjanVazirani <gunjanvazirani2001@gmail.com>
2021-07-05 11:15:58 +0530

    New Commit

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const app = express();
 const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
+const pug=require('pug');
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','pug');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
