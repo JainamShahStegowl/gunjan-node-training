b6be1d2efa8631bb6f44b0b14cfd88608fe807f4
GunjanVazirani <gunjanvazirani2001@gmail.com>
2021-07-23 10:41:53 +0530

    gitignore

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const loginrouter = express.Router();
 const loginController = require('../controller/loginController');
+
 loginrouter.get('/', loginController.loginpage)
 loginrouter.post('/submit', loginController.submit)
 loginrouter.post('/logout', loginController.logout)
